8e910b75526208d60c5bbf49d2bc1ab988200596
Jaymin Munjani <95556976+JayminMunjani@users.noreply.github.com>
2024-01-05 19:46:59 +0530

    add authorization in api

--- src/schema/user/resolver.js
+++ src/schema/user/resolver.js
@@ -1,11 +1,12 @@
 import { combineResolvers } from "graphql-resolvers";
+import { isAuthenticated } from "../../authentication";
 
 export const userQuery = {
-	getUser: combineResolvers((parent, { id }, { me }) => {}),
+	getUser: combineResolvers(isAuthenticated, (parent, { id }, { me }) => {}),
 };
 
 export const userMutation = {
-	createUser: combineResolvers((parent, { input }, { me, models }) => {
+	createUser: combineResolvers(isAuthenticated, (parent, { input }, { me, models }) => {
 		// return new Promise((resolve, reject) => {});
 	}),
 };
